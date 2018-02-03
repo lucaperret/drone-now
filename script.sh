d7251ac257e7c1d1400d0252fa71077f53673d87
Luca Perret <perret.luca@gmail.com>
2018-02-03 16:27:33 +0100

    make it work

--- script.sh
+++ script.sh
@@ -1,54 +1,49 @@
 #!/bin/sh
 set -e
 
-nowBin="./node_modules/.bin/now"
-
-NOW_OPTIONS=""
-
-if [ -n "$PLUGIN_GIT_REPOSITORY" ]
-then
-    NOW_OPTIONS+=" $PLUGIN_GIT_REPOSITORY"
-fi
-
-NOW_OPTIONS+=" --token $PLUGIN_TOKEN --no-clipboard"
+NOW_TOKEN_OPTION="--token=$PLUGIN_TOKEN"
+NOW_DEPLOY_OPTIONS="${NOW_TOKEN_OPTION} --no-clipboard"
 
 if [ -n "$PLUGIN_NAME" ]
 then
-    NOW_OPTIONS+=" --name $PLUGIN_NAME"
+    NOW_DEPLOY_OPTIONS="${NOW_DEPLOY_OPTIONS} --name ${PLUGIN_NAME}"
 fi
 
 if [ -n "$PLUGIN_TYPE" ]
 then
-    NOW_OPTIONS+=" --$PLUGIN_TYPE"
+    NOW_DEPLOY_OPTIONS="${NOW_DEPLOY_OPTIONS} --${PLUGIN_TYPE}"
 fi
 
 if [ -n "$PLUGIN_TOKEN" ]
 then
-    echo "> Deploying on now.sh…"
-    NOW_DEPLOYMENT_URL=`$nowBin$NOW_OPTIONS`
-    echo "> Success! Deployment complete to $NOW_DEPLOYMENT_URL!"
+    echo "> Deploying on now.sh…" &&
+    NOW_DEPLOYMENT_URL=$(now $NOW_DEPLOY_OPTIONS) &&
+    echo "> Success! Deployment complete to $NOW_DEPLOYMENT_URL!";
 else
     echo "> Error! NOW_TOKEN is required"
 fi
 
 if [ -n "$PLUGIN_ALIAS" ]
 then
-    ALIAS_SUCCESS_MESSAGE=`$nowBin alias $NOW_DEPLOYMENT_URL $PLUGIN_ALIAS --token $PLUGIN_TOKEN`
-    echo "$ALIAS_SUCCESS_MESSAGE"
-    NOW_DEPLOYMENT_URL=$NOW_ALIAS
+    echo "> Assigning alias…" &&
+    ALIAS_SUCCESS_MESSAGE=$(now alias $NOW_DEPLOYMENT_URL $PLUGIN_ALIAS $NOW_TOKEN_OPTION) &&
+    echo "$ALIAS_SUCCESS_MESSAGE" &&
+    NOW_DEPLOYMENT_URL="https://$PLUGIN_ALIAS";
 fi
 
 if [ -n "$PLUGIN_SCALE" ]
 then
-    SCALE_SUCCESS_MESSAGE=`$nowBin scale $NOW_DEPLOYMENT_URL $PLUGIN_SCALE --token $PLUGIN_TOKEN`
-    echo "$SCALE_SUCCESS_MESSAGE"
+    echo "> Scaling…" &&
+    SCALE_SUCCESS_MESSAGE=$(now scale $NOW_DEPLOYMENT_URL $PLUGIN_SCALE $NOW_TOKEN_OPTION) &&
+    echo "$SCALE_SUCCESS_MESSAGE";
 fi
 
 if [ -n "$PLUGIN_RULES_DOMAIN" ] && [ -n "$PLUGIN_RULES_FILE" ]
 then
-    RULES_SUCCESS_MESSAGE=`$nowBin alias $PLUGIN_RULES_DOMAIN -r $PLUGIN_RULES_FILE --token $PLUGIN_TOKEN`
-    echo "$RULES_SUCCESS_MESSAGE"
-    NOW_DEPLOYMENT_URL=$NOW_ALIAS
+    echo "> Assigning domain rules…" &&
+    RULES_SUCCESS_MESSAGE=$(now alias $PLUGIN_RULES_DOMAIN -r $PLUGIN_RULES_FILE $NOW_TOKEN_OPTION) &&
+    echo "$RULES_SUCCESS_MESSAGE" &&
+    NOW_DEPLOYMENT_URL=$PLUGIN_ALIAS;
 fi
 
 echo $'\n'"> Successfully deployed! $NOW_DEPLOYMENT_URL"$'\n'
